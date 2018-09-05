1d87882889b1957b9bda9e84ff69f3f70db5ee9b
Vithalani Dhaval <dhaval.vithalani@scania.com>
2018-09-05 16:01:53 +0200

    Initial commit

--- heap-dump-data-collector.sh
+++ heap-dump-data-collector.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+export AWS_DEFAULT_REGION=eu-west-1
+export CLASSIC_LOADBALANCER=$1
+
+wait_time=10
 instance_id=$( /opt/aws/bin/ec2-metadata -i | awk '{print $2}' )
 echo ${instance_id}
-export AWS_DEFAULT_REGION=eu-west-1
 
 date=$(date +"%m-%d-%y")
 echo $date
@@ -14,13 +17,13 @@ then
         if [ -f "$file" ]
         then
                         echo "$file found."
-                        sleep 1m
-                        echo "waited 10 minutes"
-                        response=$(aws s3 cp /opt/app/heap-dump.hprof s3://data39059812/${date}/${instance_id}/)
-                        echo "Upload successfully" ${response}
-                        aws elb deregister-instances-from-load-balancer --load-balancer-name my-loadbalancer --instances ${instance_id}
+                        sleep ${wait_time}s
+                        echo "waited $wait_time minutes"
+                        response=$(aws s3 cp /opt/app/heap-dump.hprof s3://data39059812/${date}/${instance_id}/ --output text)
+                        echo $response
+                        aws elb deregister-instances-from-load-balancer --load-balancer-name ${CLASSIC_LOADBALANCER} --instances ${instance_id}
                         aws elb delete-load-balancer --load-balancer-name my-loadbalancer
-                        #aws ec2 terminate-instances --instance-ids ${instance_id}
+                        aws ec2 terminate-instances --instance-ids ${instance_id}
         else
                         echo "$file not found."
         fi
